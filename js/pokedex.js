38ddf81985b72766ded1be6f28e31c15b0ad6e10
DbsEngineering <piranna56@yahoo.fr>
2018-01-29 02:02:46 +0100

    Question 8
    Button check

--- js/pokedex.js
+++ js/pokedex.js
@@ -9,14 +9,11 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 
 pokeApp.controller('myCtrl', function($scope) {
     //Question 3 et 4
-    //var id = ["1","2","3","4","5"];
-    //$scope.id = {id};
     $scope.names = [{id:0, name:""},{id:1,name:"Mewtwo"},
     {id:2, name:"Aspicot"},{id:3, name:"Fermite"},
     {id:4,name:"Chenipan"},{id:5,name:"Aeromite"}];
-    /*var valeur = ["","Mewtwo","Aspicot","Fermite",
-    "Chenipan","Aeromite"];
-    $scope.name = valeur;*/
-    /*$scope.namepoke = ["Mewtwo","Aspicot","Fermite",
-    "Chenipan","Aeromite"];*/
+    $scope.check = function (id) {
+        console.log(id);
+        console.log($scope.names[id]);
+    }
 });
