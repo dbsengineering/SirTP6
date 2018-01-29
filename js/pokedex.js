b2953c85573c70887596d9bc45fae452de1de660
DbsEngineering <piranna56@yahoo.fr>
2018-01-29 01:17:15 +0100

    Question 6
    Ajout d'un filtre input

--- js/pokedex.js
+++ js/pokedex.js
@@ -8,5 +8,15 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 }]);
 
 pokeApp.controller('myCtrl', function($scope) {
-    $scope.namepoke = "Mewtwo";
+    //Question 3 et 4
+    //var id = ["1","2","3","4","5"];
+    //$scope.id = {id};
+    $scope.names = [{id:0, name:""},{id:1,name:"Mewtwo"},
+    {id:2, name:"Aspicot"},{id:3, name:"Fermite"},
+    {id:4,name:"Chenipan"},{id:5,name:"Aeromite"}];
+    /*var valeur = ["","Mewtwo","Aspicot","Fermite",
+    "Chenipan","Aeromite"];
+    $scope.name = valeur;*/
+    /*$scope.namepoke = ["Mewtwo","Aspicot","Fermite",
+    "Chenipan","Aeromite"];*/
 });
