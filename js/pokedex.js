f58b3946f2a95a1be778ec93f9fa8fcd73fadd0f
DbsEngineering <piranna56@yahoo.fr>
2018-01-31 01:30:36 +0100

    Q10

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,19 +1,43 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
-pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+//Service : constant. Chaque service est son propre provider. Contient des valeurs/objets
+pokeApp.constant('POKEAPI', (function(id) {
+    var resource = 'http://pokeapi.co';
+    return {
+        POKEAPI : resource + '/api/v1/pokedex/:'+id
+    }
+})());
 
-pokeApp.config(['$resourceProvider', function($resourceProvider) {
-    $resourceProvider.defaults.stripTrailingSlashes = false;
-}]);
 
-pokeApp.controller('myCtrl', function($scope) {
+
+//Controleur
+pokeApp.controller('myCtrl', function($scope, POKEAPI) {
     //Question 3 et 4
     $scope.names = [{id:0, name:""},{id:1,name:"Mewtwo"},
     {id:2, name:"Aspicot"},{id:3, name:"Fermite"},
     {id:4,name:"Chenipan"},{id:5,name:"Aeromite"}];
+
+
+    //Question 8
     $scope.check = function (id) {
-        console.log(id);
         console.log($scope.names[id]);
+    }
+
+    //Question 10
+    /*$scope.liste = function(POKEAPI){
+        POKEAPI = POKEAPI;
+        console.log(POKEAPI);
+    }*/
+    
+    $scope.changeId = function(id){
+        if(id){
+            $scope.nomPokemon = $scope.names[id];
+            console.log( $scope.nomPokemon);
+        }
+    }
+
+    $scope.getId = function(name){
+        return $scope.names[name].id;
     }
 });
