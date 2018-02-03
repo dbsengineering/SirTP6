085def2fa18ed65d4259ada64f2ef731d2f3650b
DbsEngineering <piranna56@yahoo.fr>
2018-02-03 09:37:11 +0100

    add link picture
    Add link picture and filter select with Id

--- js/pokedex.js
+++ js/pokedex.js
@@ -2,66 +2,58 @@ var pokeApp = angular.module('pokedex', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
 //Service : constant. Chaque service est son propre provider. Contient des valeurs/objets
-
-/*pokeApp.constant('$POKEAPI',{
-    resource :'http://pokeapi.co/api/v2/pokemon/'
-});*/
-
-pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+pokeApp.constant('POKEAPI', 'https://pokeapi.co');
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
 
-/*pokeApp.appService($http){
-    this.getAllPokemon = function(){
-        return $http.get(POKEAPI+'/api/v1/:pokemon');
-    };
-}*/
-
 //Controleur
 pokeApp.controller('myCtrl',function($scope, $http, POKEAPI) {
     //Question 3 et 4
-
-    $scope.names = [{id:0, name:""},{id:1,name:"Mewtwo"},
+    /*$scope.names = [{id:0, name:""},{id:1,name:"Mewtwo"},
     {id:2, name:"Aspicot"},{id:3, name:"Fermite"},
-    {id:4,name:"Chenipan"},{id:5,name:"Aeromite"}];
+    {id:4,name:"Chenipan"},{id:5,name:"Aeromite"}];*/
 
-    /*var obj = {content:null};
-
-    $http.get(POKEAPI).success(function(data) {
-        // you can do some processing here
-        obj.content = data;
-        console.log(obj.content);
-    });*/
-
+    
+    //Question 10
+    $scope.names = new Array();
     var vm = this;
-
-    $http.get(POKEAPI+'/api/v1/pokemon').success(function (listePokemon){
+    $http.get(POKEAPI+'/api/v1/pokedex').success(function (listePokemon){
         vm.listePokemon = listePokemon;
+        angular.forEach(vm.listePokemon.objects[0].pokemon, function(value, key){
+            var array = value.resource_uri.split('api/v1/pokemon/');
+            var idPok = value.resource_uri.substring(15);
+            idPok = idPok.substring(-10000, idPok.length-1);
+            $scope.names.push({id:idPok,name:value.name});
+        });
+
+        
+
     }).error(function(error){
         vm.statut = 'Impossible de récupérer la liste des pokémons : ' + error.Message;
     });
-    
-    
 
-    /*console.log($http.get(POKEAPI+'.json').success(function(response) {
-        return response.data;
-    }));*/
-
+    
     //Question 8
     $scope.check = function (id) {
-        console.log($scope.names[id]);
+        var lePokemon = this;
+        $http.get(POKEAPI+'/api/v2/pokemon-form/'+id).success(function (lePokemon){
+            lePokemon.listeCompetence = lePokemon;
+            
+            console.log(lePokemon.listeCompetence.sprites.front_default);
+            $scope.image = [{
+                src: lePokemon.listeCompetence.sprites.front_default,
+              }];
+    
+        }).error(function(error){
+            vm.statut = 'Impossible de récupérer la liste des pokémons : ' + error.Message;
+        });
+        //console.log($scope.names[id]);
     }
 
-    //Question 10
-    /*$scope.liste = function(POKEAPI){
-        $scope.POKEAPI = POKEAPI.$resource+$scope.names.id;
-        console.log($scope.POKEAPI);
-    }*/
-
-    $scope.nomPokemon="Pokémon"
+    $scope.nomPokemon=""
     //Récupère nom
     $scope.changeId = function(id){
         if(id){
