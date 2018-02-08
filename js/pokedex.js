00da63b1eaa77a69fa6e964c8986c7388fe4f250
DbsEngineering <piranna56@yahoo.fr>
2018-02-08 23:41:45 +0100

    Add filter and factory
    Add double filter and factory link API

--- js/pokedex.js
+++ js/pokedex.js
@@ -8,9 +8,18 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
+//Factory : liens API
+pokeApp.factory("resources", function(POKEAPI){
+    return{
+        listePoke:POKEAPI+'/api/v1/pokedex',
+        resPok:'api/v1/pokemon/',
+        linkPok:POKEAPI+'/api/v2/pokemon-form/',
+    }
+});
+
 
 //Controleur
-pokeApp.controller('myCtrl',function($scope, $http, POKEAPI) {
+pokeApp.controller('myCtrl',function($scope, $http, POKEAPI, resources) {
     //Question 3 et 4
     /*$scope.names = [{id:0, name:""},{id:1,name:"Mewtwo"},
     {id:2, name:"Aspicot"},{id:3, name:"Fermite"},
@@ -20,16 +29,15 @@ pokeApp.controller('myCtrl',function($scope, $http, POKEAPI) {
     //Question 10
     $scope.names = new Array();
     var vm = this;
-    $http.get(POKEAPI+'/api/v1/pokedex').success(function (listePokemon){
+
+    $http.get(resources.listePoke).success(function (listePokemon){
         vm.listePokemon = listePokemon;
         angular.forEach(vm.listePokemon.objects[0].pokemon, function(value, key){
-            var array = value.resource_uri.split('api/v1/pokemon/');
+            var array = value.resource_uri.split(resources.resPok);
             var idPok = value.resource_uri.substring(15);
             idPok = idPok.substring(-10000, idPok.length-1);
             $scope.names.push({id:idPok,name:value.name});
         });
-
-        
 
     }).error(function(error){
         vm.statut = 'Impossible de récupérer la liste des pokémons : ' + error.Message;
@@ -39,10 +47,10 @@ pokeApp.controller('myCtrl',function($scope, $http, POKEAPI) {
     //Question 8
     $scope.check = function (id) {
         var lePokemon = this;
-        $http.get(POKEAPI+'/api/v2/pokemon-form/'+id).success(function (lePokemon){
+        $http.get(resources.linkPok+id).success(function (lePokemon){
             lePokemon.listeCompetence = lePokemon;
             
-            console.log(lePokemon.listeCompetence.sprites.front_default);
+            //console.log(lePokemon.listeCompetence.sprites.front_default);
             $scope.image = [{
                 src: lePokemon.listeCompetence.sprites.front_default,
               }];
@@ -50,7 +58,6 @@ pokeApp.controller('myCtrl',function($scope, $http, POKEAPI) {
         }).error(function(error){
             vm.statut = 'Impossible de récupérer la liste des pokémons : ' + error.Message;
         });
-        //console.log($scope.names[id]);
     }
 
     $scope.nomPokemon=""
